8d4967fc1c21453b300d7e2a4015bf4f1c7d8fbb
sanya2401 <139520535+sanya2401@users.noreply.github.com>
2026-04-01 18:12:55 +1100

    Fix Firebase setup configuration (#30)

--- mobile-app/config/firebaseConfig.ts
+++ mobile-app/config/firebaseConfig.ts
@@ -8,12 +8,12 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCF7i09IXwAkYE5VGd0rgXVz_axnN2iv3c",
+  authDomain: "foodremedy-deakin.firebaseapp.com",
+  projectId: "foodremedy-deakin",
+  storageBucket: "foodremedy-deakin.firebasestorage.app",
+  messagingSenderId: "314092160331",
+  appId: "1:314092160331:web:13e345f7d961f31489d4ce",
   measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
