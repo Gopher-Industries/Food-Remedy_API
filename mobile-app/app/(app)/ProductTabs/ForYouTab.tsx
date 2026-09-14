1e0544bb48afee200d2e188e3dc3ce91790afebb
bhanuja4776 <bhanuja4776.se24@chitkara.edu.in>
2026-09-14 11:24:06 +1000

    FE038 - Improve frontend performance

--- mobile-app/app/(app)/ProductTabs/ForYouTab.tsx
+++ mobile-app/app/(app)/ProductTabs/ForYouTab.tsx
@@ -194,7 +194,7 @@ type Props = {
   product: any;
 };
 
-export default function CompareTab({ product }: Props) {
+function CompareTab({ product }: Props) {
   const { profiles, activeProfile } = useProfile();
   const { darkMode } = usePreferences();
 
@@ -236,3 +236,5 @@ export default function CompareTab({ product }: Props) {
     </View>
   );
 }
+
+export default React.memo(CompareTab);
